56100eba408f016a4afdd42deccfecaf7b6d357d
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-02-07 21:38:31 +0200

    Another fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -9,6 +9,6 @@ def filter_datum(fields: List[int], redaction: str,
                  message: str, separator: str) -> str:
     """Returns the log message obfuscated."""
     for field in fields:
-        message = re.sub(rf"{field}=.*?{separator}",
+        message = re.sub(rf"{field}=.+?{separator}",
                          f"{field}={redaction}{separator}", message)
     return message
