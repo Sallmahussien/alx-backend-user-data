2f57c298ceb3ecd88ff0e9a0fb253eae122c48c2
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-02-07 22:26:55 +0200

    Add type annotations for `__init__` method

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -19,14 +19,13 @@ def filter_datum(fields: List[str], redaction: str,
 
 class RedactingFormatter(logging.Formatter):
     """ Redacting Formatter class
-        """
+    """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields):
-        """Initialize a new object"""
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
