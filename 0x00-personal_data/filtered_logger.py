0d157c87c83d41fa57b7ee145866599ba5d41c2b
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-02-07 21:40:39 +0200

    Edit the type annotation

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,7 @@ from typing import List
 import re
 
 
-def filter_datum(fields: List[int], redaction: str,
+def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """Returns the log message obfuscated."""
     for field in fields:
