029c239a4e4a8c2c5d081e0f2b448a4d65a2dd48
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-02-07 22:22:55 +0200

    Update the docstring

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -26,10 +26,11 @@ class RedactingFormatter(logging.Formatter):
     SEPARATOR = ";"
 
     def __init__(self, fields):
+        
         super(RedactingFormatter, self).__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """Format method"""
+        """Formats a LogRecord."""
         msg = super(RedactingFormatter, self).format(record)
         return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
