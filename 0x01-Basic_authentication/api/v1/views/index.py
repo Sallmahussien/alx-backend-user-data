17c682222ed87c6830b1e36890f169acc2893ded
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-02-12 20:58:29 +0200

    Fix pycodestyle

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -5,7 +5,7 @@ from flask import jsonify, abort
 from api.v1.views import app_views
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes = False)
+@app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
     """ GET /api/v1/status
     Return:
@@ -14,7 +14,7 @@ def status() -> str:
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats/', strict_slashes = False)
+@app_views.route('/stats/', strict_slashes=False)
 def stats() -> str:
     """ GET /api/v1/stats
     Return:
@@ -26,14 +26,14 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes = False)
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
 def unauthorized() -> str:
     """GET /api/v1/unauthorized
     Return: error msg"""
     abort(401)
 
 
-@app_views.route('forbidden', strict_slashes = False)
+@app_views.route('forbidden', strict_slashes=False)
 def forbidden() -> str:
     """GET /api/v1/forbidden
     Return: forbidden err"""
