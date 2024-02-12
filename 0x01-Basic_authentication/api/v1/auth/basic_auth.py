e09eca69f7980fe5fa66176efb90f35512998bbf
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-02-12 23:35:25 +0200

    Add task 7

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -8,3 +8,13 @@ class BasicAuth(Auth):
     """Implement BasicAuth class"""
     def __init__(self) -> None:
         super().__init__()
+
+    def extract_base64_authorization_header(self,
+                                            authorization_header: str) -> str:
+        """Extract base64 from the header"""
+        if (authorization_header is None
+                or type(authorization_header) is not str
+                or not authorization_header.startswith('Basic ')):
+            return None
+
+        return authorization_header[6:]
