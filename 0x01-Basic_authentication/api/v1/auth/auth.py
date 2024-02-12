8ba68742f33890ebf69720cb0beba7e64e91e3dc
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-02-12 21:47:25 +0200

    Add task 1

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -10,7 +10,16 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """Require auth"""
-        return False
+        if path is None or excluded_paths is None or excluded_paths == []:
+            return True
+
+        if not path.endswith('/'):
+            path = path + '/'
+
+        if path in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Authrization header"""
