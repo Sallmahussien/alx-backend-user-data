6a5149d0ffdbe538147d33e0cace989156fe749b
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-02-20 20:45:53 +0200

    Handle `NoResultFound` and `InvalidRequestError` exception

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -5,7 +5,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound
+from sqlalchemy.exc import NoResultFound, InvalidRequestError
 
 from user import Base, User
 
@@ -40,9 +40,14 @@ class DB:
 
     def find_user_by(self, **kwargs) -> User:
         """Find user based on the input arguments"""
-        user = self._session.query(User).filter_by(**kwargs).first()
+        try:
+            user = self._session.query(User).filter_by(**kwargs).first()
 
-        if not user:
-            raise NoResultFound
+            if not user:
+                raise NoResultFound
 
-        return user
+            return user
+        except NoResultFound:
+            raise
+        except InvalidRequestError:
+            raise
