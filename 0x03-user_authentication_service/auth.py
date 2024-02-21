954b72196da12037d866556741ef25c998169cd7
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-02-21 11:44:29 +0200

    Gets user by session id

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -46,6 +46,17 @@ class Auth:
             return session_id
         except NoResultFound:
             return None
+        
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """Returns user object based on session_id"""
+        if not session_id:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
 
 
 def _hash_password(password: str) -> bytes:
