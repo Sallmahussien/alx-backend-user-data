a30052cf1763acfd793c75e6a6cb4d48702cf8fb
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-02-21 17:35:26 +0200

    Update `reset_token` attribute instead of `session_id`

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -72,9 +72,9 @@ class Auth:
         """Generate reset password token"""
         try:
             user = self._db.find_user_by(email=email)
-            session_id = _generate_uuid()
-            self._db.update_user(user.id, session_id=session_id)
-            return session_id
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
         except NoResultFound:
             raise ValueError
 
