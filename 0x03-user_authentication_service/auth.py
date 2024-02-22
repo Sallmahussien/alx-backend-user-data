0689cd6002c0ef579ccedb4fee37609380eb597d
Salma Hussien <107444767+Sallmahussien@users.noreply.github.com>
2024-02-22 16:26:38 +0200

    Fix `update_password` method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -79,17 +79,15 @@ class Auth:
             raise ValueError
 
     def update_password(self, reset_token: str, password: str) -> None:
-        """hash the password and update the user's hashed_password
-        and reset_token database fields"""
+        """Hashes password and upadtes it in db"""
         try:
             user = self._db.find_user_by(reset_token=reset_token)
+            new_hashed_password = _hash_password(password)
+            self._db.update_user(user.id,
+                                 hashed_password=new_hashed_password,
+                                 reset_token=None)
         except NoResultFound:
-            raise ValueError
-
-        hashed_password = _hash_password(password)
-        self._db.update_user(user.id, hashed_password=hashed_password,
-                             reset_token=None)
-        return None
+            raise ValueError()
 
 
 def _hash_password(password: str) -> bytes:
